6c67cd36b4679ab79019b5ba3ba53ccbd9791294
NWrenger <nils@wrenger.net>
2023-03-25 00:56:23 +0100

    :wrench: Changed Code Format

--- src/generation.rs
+++ src/generation.rs
@@ -1,9 +1,9 @@
 use std::u8;
 
-use rand::{prelude::*};
-use rand::rngs::adapter::{ReseedingRng};
-use rand_chacha::ChaCha20Core;
+use rand::prelude::*;
+use rand::rngs::adapter::ReseedingRng;
 use rand::rngs::OsRng;
+use rand_chacha::ChaCha20Core;
 
 pub fn seeder(seed: u8) -> u8 {
     let s: u8 = if seed != 0 {
@@ -17,6 +17,11 @@ pub fn seeder(seed: u8) -> u8 {
 
 pub fn generator(seed: u8) -> Vec<u8> {
     let mut rng = ReseedingRng::new(ChaCha20Core::seed_from_u64(seed.into()), 0, OsRng);
-    let nums: Vec<u8>= vec![rng.gen_range(1..4), rng.gen_range(1..4), rng.gen_range(1..4), rng.gen_range(1..4)];
+    let nums: Vec<u8> = vec![
+        rng.gen_range(1..4),
+        rng.gen_range(1..4),
+        rng.gen_range(1..4),
+        rng.gen_range(1..4),
+    ];
     nums
 }
